872adc97ad28b0e9d2f0aac9f94f15d9a55ab392
pavanraocr <pavanraocr@gmail.com>
2016-09-25 02:16:11 -0500

    completed parsing and setting the shell directory

--- myshell/headers/customShell.h
+++ myshell/headers/customShell.h
@@ -8,7 +8,16 @@
 #ifndef HEADERS_CUSTOMSHELL_H_
 #define HEADERS_CUSTOMSHELL_H_
 
-extern char *SHELL_HOME;
+#ifdef MAIN_FILE
+	char *SHELL_HOME;
+	char *PATH_VAR[20];
+	int NUM_OF_PATHS;
+#else
+	extern char *SHELL_HOME;
+	extern char *PATH_VAR[20];
+	extern int NUM_OF_PATHS;
+#endif
+
 typedef enum statusType{EXIT = 0, ERR = 1, COMPLETED=2}statusType;
 
 
